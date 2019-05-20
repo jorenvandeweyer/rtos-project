f06ae0a797663000379d95efcad8edbf3e5c6413
Pieter-Jan Buntinx <pieterjan.buntinx@telenet.be>
2019-05-21 01:20:38 +0200

    final version, PID values can still be tuned

--- Balancing_arm/Balancing_arm/Balancing_arm.ino
+++ Balancing_arm/Balancing_arm/Balancing_arm.ino
@@ -12,32 +12,36 @@
 #include <Wire.h>
 #include <Servo.h>
 
-const float KP = 3.55;
-const float KI = 0.005;
-const float KD = 2.05;
-const int DEADZONE = 128;
+#define RAD_TO_DEG (180/PI)
+#define DEADZONE 128
 
-void TaskGyroscope( void *pvParameters );
-void TaskESC( void *pvParameters );
+float KP = 3.55;
+float KI = 0.005;
+float KD = 2.05;
 
-volatile float desired_angle = 0;
-float rad_to_deg = 180/3.141592654;
+float desired_angle = 0;
 float total_angle_x = 0;
-float total_angle_y = 0;
-float previous_error;
+//float total_angle_y = 0;
+
+//float angle_a_x = 0;
+//float angle_a_y = 0;
+
+bool armed = false;
+
 float PID_p;
 float PID_i;
 float PID_d;
+float previous_error;
 
 void setup() {
-	Serial.begin(115200);
+	Serial.begin(9600);
 
 	xTaskCreate(
 	TaskIMU,
 	(const portCHAR *)"IMU",
-	384,  // Stack size
+	300,  // Stack size
 	NULL,
-	1,  // priority
+	2,  // priority
 	NULL
 	);
 
@@ -46,7 +50,7 @@ void setup() {
 	(const portCHAR *)"Serial",
 	128,
 	NULL,
-	2,
+	1,
 	NULL
 	);
 }
@@ -62,23 +66,63 @@ void loop() {
 void TaskSerial(void *pvParameters) {
 	(void) pvParameters;
 
-	int val = 0;
-	
 	vTaskDelay( 1000 / portTICK_PERIOD_MS );
 
 	for(;;) {
 		if (Serial.available() > 0) {
-			char inputBuffer[16];
-			Serial.readBytes(inputBuffer, sizeof(inputBuffer));
-			val = atoi(inputBuffer);
-			if (val < -5) val = -5;
-			if (val > 70) val = 70;
-			desired_angle = val;
-			Serial.print("Desired angle: "); Serial.println(desired_angle);
+			char character = Serial.read();
+			
+			float value;
+			switch (character)
+			{
+				case 'O':
+					if (Serial.read() != '/') break;
+					armed = !armed;
+					if (armed) {
+						Serial.println("The balancing arm is now armed!");
+					} else {
+						Serial.println("The balancing arm is now disarmed!");
+					}
+					vTaskDelay( 1000 / portTICK_PERIOD_MS );
+					break;
+				case 'A':
+					value = Serial.parseFloat();
+					if (Serial.read() != '/') break;
+					Serial.print("Changing desired angle to: "); Serial.println(value);
+					if (value < -5) value = -5;
+					if (value > 70) value = 70;
+					desired_angle = value;
+					vTaskDelay( 1000 / portTICK_PERIOD_MS );
+					break;
+				case 'P':
+					value = Serial.parseFloat();
+					if (Serial.read() != '/') break;
+					Serial.print("Changing P gain to: "); Serial.println(value);
+					KP = value;	
+					vTaskDelay( 1000 / portTICK_PERIOD_MS );
+					break;
+				case 'I':
+					value = Serial.parseFloat();
+					if (Serial.read() != '/') break;
+					Serial.print("Changing I gain to: "); Serial.println(value);
+					KI = value;
+					vTaskDelay( 1000 / portTICK_PERIOD_MS );
+					break;
+				case 'D':
+					value = Serial.parseFloat();
+					if (Serial.read() != '/') break;
+					Serial.print("Changing D gain to: "); Serial.println(value);
+					KD = value;
+					vTaskDelay( 1000 / portTICK_PERIOD_MS );
+					break;
+					
+			}
 			Serial.flush();
 		}
-
-		vTaskDelay( 500 / portTICK_PERIOD_MS );
+		
+		Serial.print("Desired angle: "); Serial.print(desired_angle);
+		Serial.print(" current angle: "); Serial.println(total_angle_x);
+		vTaskDelay(1);
 	}
 }
 
@@ -93,23 +137,19 @@ void TaskIMU(void *pvParameters) {
     L3G4200D gyroscope;
     Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
-    Serial.println("HALLOOOOO");
+    unsigned long last_time = 0;
 
-    unsigned long last_time = millis();
-
-    while(!gyroscope.begin(L3G4200D_SCALE_2000DPS, L3G4200D_DATARATE_400HZ_50)) {
+    while(!gyroscope.begin(L3G4200D_SCALE_2000DPS, L3G4200D_DATARATE_100HZ_12_5)) {
 	    Serial.println("Could not find a valid L3G4200D sensor, check wiring!");
-	    vTaskDelay( 500 / portTICK_PERIOD_MS ); // wait for one second
+	    vTaskDelay( 1000 / portTICK_PERIOD_MS );
     }
 
     while (!accel.begin()) {
 	    Serial.println("Could not find a valid ADXL345 sensor, check wiring!");
-	    vTaskDelay( 500 / portTICK_PERIOD_MS );
+	    vTaskDelay( 1000 / portTICK_PERIOD_MS );
     }
 
     gyroscope.calibrate(100);
-    accel.setRange(ADXL345_RANGE_16_G);
-    
 
     for (;;) {
 	    sensors_event_t event;
@@ -119,16 +159,17 @@ void TaskIMU(void *pvParameters) {
 	    
 	    unsigned long timer = millis();
 	    float timeStep = (float)(timer-last_time)/1000;
-	    last_time = timer;
 	    
 		//Calculate angles from accelerometer data using Euler formula
-		float angle_a_x = atan((event.acceleration.y)/sqrt(pow((event.acceleration.x),2) + pow((event.acceleration.z),2)))*rad_to_deg;
-		float angle_a_y = atan(-1*(event.acceleration.x)/sqrt(pow((event.acceleration.y),2) + pow((event.acceleration.z),2)))*rad_to_deg;
+		float angle_a_x = event.orientation.y*9; 
+		//value is too small by a factor of 9 for some reason, should normally return 
+		//orientation of y axis in degrees
 
 		//Complementary filter to combine gyroscope and accelerometer angles
-		float total_angle_x = 0.98 *(total_angle_x + (norm.XAxis * timeStep)) + 0.02*angle_a_x;
-		float total_angle_y = 0.98 *(total_angle_y + (norm.YAxis * timeStep)) + 0.02*angle_a_y;
+		total_angle_x = 0.98 *(total_angle_x + (norm.XAxis * timeStep)) + 0.02*angle_a_x;
+		//total_angle_y = 0.98 *(total_angle_y + (norm.YAxis * timeStep)) + 0.02*angle_a_y;
 		
+		//float error = total_angle_x - desired_angle;
 		float error = total_angle_x - desired_angle;
 		
 		PID_p = KP*error;
@@ -145,16 +186,14 @@ void TaskIMU(void *pvParameters) {
 		if (PID < 0) PID = 0;
 		if (PID > 1000) PID = 1000;
 		
-		ESC.writeMicroseconds(1000 + DEADZONE + PID);
-		
-		/**Serial.print("Desired angle: "); Serial.print(desired_angle);
-	    Serial.print(" Accel: x: "); Serial.print(angle_a_x);
-	    Serial.print("  y: ");Serial.print(angle_a_y);
-		Serial.print(" Total angle: X: "); Serial.print(total_angle_x);
-		Serial.print(" Y: "); Serial.print(total_angle_y);
-		Serial.print(" Throttle value (PID): "); Serial.println(PID);*/
+		if (armed) {
+			ESC.writeMicroseconds(1000 + DEADZONE + PID);
+		} else {
+			ESC.writeMicroseconds(1000);
+		}
 		
 		previous_error = error;
-	    vTaskDelay( 20 / portTICK_PERIOD_MS );
+		last_time = millis();
+	    vTaskDelay(1);
     }
 }
